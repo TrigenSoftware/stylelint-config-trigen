19447fd1497f42acc3d5e708f06dbc3e17c1f192
Dan Green <danon0404@gmail.com>
2018-06-23 21:49:35 +0700

    a11y rules fix

--- rules/common.js
+++ rules/common.js
@@ -199,7 +199,7 @@ module.exports = {
 		'a11y/media-prefers-reduced-motion':                 true,
 		'a11y/no-outline-none':                              true,
 		'a11y/selector-pseudo-class-focus':                  true,
-		'a11y/no-obsolete-attribute':                        true,
+		// 'a11y/no-obsolete-attribute':                        true,
 		'a11y/no-obsolete-element':                          true,
 		'a11y/no-text-align-justify':                        true,
 		'a11y/font-size-is-readable':                       [true, { 'severity': 'warning' }],
